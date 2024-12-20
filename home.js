5d919eee076e2880d8f4be18265fbeb92d9ca9b7
geisianefgs <geisianefgs@gmail.com>
2024-12-19 21:56:34 -0300

    commit

---
+++ home.js
@@ -0,0 +1,8 @@
+function clickmenu(){
+    homelinks = document.getElementById('homelinks')
+        if (homelinks.style.display == 'block'){
+            homelinks.style.display = 'none'
+        } else{
+            homelinks.style.display = 'block'
+        }
+}
